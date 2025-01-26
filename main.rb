c7dd3d409b64807edf043cbb83b78b4108e48b41
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-26 17:24:16 +0100

    Changed the case condition from logic Because Ruby does not support logic inside case Therefore I used range 65..90 I used ASCII table to get each character to Integer and added the 5 shift to get the new char

--- main.rb
+++ main.rb
@@ -1,3 +1,18 @@
 def caesar_cipher(phrase, shift)
-  
+  result = ""
+  phrase.each_byte do |byte|
+    new_byte = byte
+    case byte
+      when 65..90
+        new_byte = byte + shift
+        new_byte -= 26 if new_byte > 90
+      when 97..122
+        new_byte = byte + shift
+        new_byte -= 26 if new_byte > 122
+    end
+    result << new_byte.chr
+  end
+  puts result
 end
+
+caesar_cipher("What a string!", 5)
