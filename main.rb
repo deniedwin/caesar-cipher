a1a21deb6912aecdb0353bd817400d83f0ee33f5
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-27 15:47:39 +0100

    Added more comments for clarity

--- main.rb
+++ main.rb
@@ -1,19 +1,25 @@
+# custom method to transform a string into a encrypted 
+# string using caesar cipher by shifting to the right
 def caesar_cipher(phrase, shift)
   # string to store the new shifted string
   result = ""
   # method to get ascii number for each character
   phrase.each_byte do |byte|
-    # new byte to use to for shift later
+    # new byte to encrypt by shift
     new_byte = byte
-    # case for two possibilities either lower or uppercaps
+    # case for two possibilities either upper or lowercase
     case byte
-      # for upper letters
+      # for uppercase
       when 65..90
+        # shift (or encrypt) the letter to the right by shift value
         new_byte = byte + shift
+        # wrap around when the letter hit "Z"
         new_byte -= 26 if new_byte > 90
-      # for lower letters
+      # for lowercase
       when 97..122
+        # shift (or encrypt) the letter to the right by shift value
         new_byte = byte + shift
+        # wrap around when the letter hit "z"
         new_byte -= 26 if new_byte > 122
     end
     # store all new chars in new string
