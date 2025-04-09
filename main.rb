367e4a02dee204e1328f690e3d5466a710f1a16b
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-04-09 16:57:21 +0200

    Configured bundle and gems to work with RuboCop

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
-# custom method to transform a string into a encrypted 
+# frozen_string_literal: true
+
+# custom method to transform a string into a encrypted
 # string using caesar cipher by shifting to the right
 def caesar_cipher(phrase, shift)
   # string to store the new shifted string
-  result = ""
+  result = ''
   # method to get ascii number for each character
   phrase.each_byte do |byte|
     # new byte to encrypt by shift
@@ -10,17 +12,17 @@ def caesar_cipher(phrase, shift)
     # case for two possibilities either upper or lowercase
     case byte
       # for uppercase
-      when 65..90
-        # shift (or encrypt) the letter to the right by shift value
-        new_byte = byte + shift
-        # wrap around when the letter hit "Z"
-        new_byte -= 26 if new_byte > 90
+    when 65..90
+      # shift (or encrypt) the letter to the right by shift value
+      new_byte = byte + shift
+      # wrap around when the letter hit "Z"
+      new_byte -= 26 if new_byte > 90
       # for lowercase
-      when 97..122
-        # shift (or encrypt) the letter to the right by shift value
-        new_byte = byte + shift
-        # wrap around when the letter hit "z"
-        new_byte -= 26 if new_byte > 122
+    when 97..122
+      # shift (or encrypt) the letter to the right by shift value
+      new_byte = byte + shift
+      # wrap around when the letter hit "z"
+      new_byte -= 26 if new_byte > 122
     end
     # store all new chars in new string
     result << new_byte.chr
@@ -30,4 +32,4 @@ def caesar_cipher(phrase, shift)
 end
 
 # call the method
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
